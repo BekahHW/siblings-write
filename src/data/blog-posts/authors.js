7b8aa5f4b8a17ba9b9204eb4227b19d005da7b7c
BekahHW <rhawrot@gmail.com>
2025-02-02 19:08:57 -0500

    Simplify author page

--- src/data/blog-posts/authors.js
+++ src/data/blog-posts/authors.js
@@ -1,20 +1,17 @@
 export const authors = {
-    Bekah: {
-      name: "Bekah",
+    BekahHW: {
+      name: "BekahHW",
       bio: "Bekah grew up in a storytelling-rich environment with her brothers, where books and imagination were central to daily life. Her love of storytelling, nurtured since childhood and refined through her MA in English, now finds expression across multiple writing forms. A mother of four, she continues to encourage the same spirit of everyday magic and adventure with her own children.",
-      avatar: "/assets/authors/bekah-avatar.webp",
-      link: "/authors/bekah",
+      // avatar: "/assets/authors/bekah-avatar.webp",
     },
     Josh: {
       name: "Josh",
       bio: "Josh Hawrot is married to his inspiring wife Cruz with whom they have 5 out-of-this-world kids! A husband, father and pastoral counselor by day (and night as well) come night (or whenever there happens to be a spare moment!) he dawns his writer's cape and loves creating stories and worlds with his siblings. He also loves his faith and his mill hometown of which he has strong roots.",
-      avatar: "/assets/authors/josh-avatar.webp",
-      link: "/authors/josh",
+      // avatar: "/assets/authors/josh-avatar.webp",
     },
     Zach: {
       name: "Zach",
       bio: "Zach Hawrot is a writer from a small town, a husband to his high school flame (that’s right, Amanda), and a father of four beautiful — and sometimes feral — children. After writing spec screenplays for over ten years, he decided to write a book — The Wild Adventure of Mitch and the Sand Bridge",
-      avatar: "/assets/authors/zach-avatar.webp",
-      link: "/authors/zach",
+      // avatar: "/assets/authors/zach-avatar.webp",
     },
   };
