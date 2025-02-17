755d32bedba0501e5da1b7040139ca9b5d66b293
BekahHW <rhawrot@gmail.com>
2025-02-17 14:26:39 -0500

    Add joshs headshot

--- src/data/blog-posts/authors.js
+++ src/data/blog-posts/authors.js
@@ -7,7 +7,7 @@ export const authors = {
     Josh: {
       name: "Josh",
       bio: "Josh Hawrot is married to his inspiring wife Cruz with whom they have 5 out-of-this-world kids! A husband, father and pastoral counselor by day (and night as well) come night (or whenever there happens to be a spare moment!) he dawns his writer's cape and loves creating stories and worlds with his siblings. He also loves his faith and his mill hometown of which he has strong roots.",
-      // avatar: "/assets/josh-avatar.webp",
+      avatar: "/assets/josh.jpg",
     },
     Zach: {
       name: "Zach",
